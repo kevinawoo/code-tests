04610224d80c64079dad7cea522e3fc00359d7b6
Kevin Woo <kevinawoo@gmail.com>
2014-12-30 21:10:22 -0800

    fizzbuzz

---
+++ FizzBuzz/fizzbuzz.js
@@ -0,0 +1,46 @@
+'use strict';
+
+
+// Write a program that prints the numbers from 1 to 100.
+// But for multiples of three print “Fizz” instead of the number and
+// for the multiples of five print “Buzz”.
+// For numbers which are multiples of both three and five print “FizzBuzz”
+
+function FizzBuzzMultipleStatements() {
+	for (var i = 1; i <= 100; i++) {
+		if (i % 3 === 0 && i % 5 === 0) {
+			console.log('FizzBuzz');
+		}
+		else if (i % 3 === 0) {
+			console.log('Fizz');
+		}
+		else if (i % 5 === 0) {
+			console.log('Buzz');
+		}
+		else {
+			console.log(i);
+		}
+	}
+}
+
+
+function FizzBuzzSingleLine() {
+	var output = '';
+
+	for (var i = 1; i < 100; i++) {
+		if (i % 3 === 0) {
+			output += 'Fizz';
+		}
+
+		if (i % 5 === 0) {
+			output += 'Buzz';
+		}
+
+		console.log(output || i);
+		output = '';
+	}
+}
+
+
+FizzBuzzMultipleStatements();
+FizzBuzzSingleLine();
